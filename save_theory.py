ce8bbd227cf0f39716bd31f0cf82f5f1d09f7f52
Joe Zuntz <joezuntz@googlemail.com>
2019-12-06 15:18:30 +0000

    add docstrings

--- save_theory.py
+++ save_theory.py
@@ -1,3 +1,11 @@
+"""
+Save theory points to a text file.
+
+Note that adding this to the end of a pipeline will save *all* evaluated
+points, not just accepted ones.  You will have to compare to the final
+chain file to determine which points are accepted and which are not.
+"""
+
 from cosmosis.datablock import option_section
 from cosmosis.runtime.config import Inifile
 import numpy as np
@@ -26,52 +34,67 @@ def setup(options):
     else:
         likes = None
 
-
+    # Work out the varied parameters
+    # Hackish to do this again, but it's not exposed here otherwise
     values_file = options['pipeline', 'values']
     values_ini = Inifile(values_file)
 
+    # Work out which ones are varied and which are fixed
     varied_params = []
     for ((section, name), value) in values_ini:
         words = value.split()
         if len(words) == 3:
             varied_params.append((section,name))
+
+    # Write file headers (just param names here,
+    # data elements below)
     outfile.write('#')
     for (section,name) in varied_params:
         outfile.write('{}--{}  '.format(section,name))
-    
+
+    # For info    
     print(varied_params)
 
     return [outfile, likes, varied_params]
 
 def execute(block, config):
 
+    # We will modify config later by adding an element
+    # to indicate that the header has been completed
     (outfile, likes, varied_params) = config[:3]
 
+    # Build up output line, starting with
+    # input param values
     data = []
     for (section, name) in varied_params:
         data.append(block[section, name])
     data = np.array(data)
 
-    # 
+    # If left blank (=automatic) then write determine
+    # which likelihoods to use
     if likes is None:
         likes = []
         for _, key in block.keys('data_vector'):
             if key.endswith('_theory'):
                 likes.append(key[:-7])
 
+    # Get all the theory vectors
     for like in likes:
         theory = block['data_vector', like+"_theory"]
         data = np.append(data, theory)
 
+    # If this is our first execution, write the rest of
+    # the header line d_0  d_1  d_2 ...
     if len(config)==3:
         for i in range(len(data) - len(varied_params)):
             outfile.write('d_{}  '.format(i))
         outfile.write('\n')
         config.append(True)
 
+    # Save output line - made 2D so it is saved as a single
+    # line
     data = np.atleast_2d(data)
     np.savetxt(outfile, data)
-
     outfile.flush()
 
     return 0
